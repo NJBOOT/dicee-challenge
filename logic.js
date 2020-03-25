99183295e3317228ff19458341e0b809fb8615f9
Nico Barbarese <nicobarbarese@gmail.com>
2020-03-25 14:32:20 -0400

    added click event to simulate roll

--- logic.js
+++ logic.js
@@ -1,36 +1,42 @@
-let randomNumberOne = Math.floor(Math.random()*6)+1
-let randomNumberTwo = Math.floor(Math.random()*6)+1
 
-function rollMe (randomNumber,image){
-    switch(randomNumber) {
-            case 1:
-                document.querySelector(image).setAttribute("src", "./images/dice1.png");
-               break;
-            case 2:
-                document.querySelector(image).setAttribute("src", "./images/dice2.png"); 
-                break;
-            case 3:
-                document.querySelector(image).setAttribute("src", "./images/dice3.png"); 
-                break;  
-            case 4:
-                document.querySelector(image).setAttribute("src", "./images/dice4.png"); 
-                break;  
-            case 5:
-                document.querySelector(image).setAttribute("src", "./images/dice5.png"); 
-                break;  
-            case 6:
-                document.querySelector(image).setAttribute("src", "./images/dice6.png"); 
-                break;
+
+function rollMe(randomNumber, image) {
+    switch (randomNumber) {
+        case 1:
+            document.querySelector(image).setAttribute("src", "./images/dice1.png");
+            break;
+        case 2:
+            document.querySelector(image).setAttribute("src", "./images/dice2.png");
+            break;
+        case 3:
+            document.querySelector(image).setAttribute("src", "./images/dice3.png");
+            break;
+        case 4:
+            document.querySelector(image).setAttribute("src", "./images/dice4.png");
+            break;
+        case 5:
+            document.querySelector(image).setAttribute("src", "./images/dice5.png");
+            break;
+        case 6:
+            document.querySelector(image).setAttribute("src", "./images/dice6.png");
+            break;
     }
 }
 
-rollMe(randomNumberOne, ".img1");
-rollMe(randomNumberTwo, ".img2");
 
-if (randomNumberOne === randomNumberTwo) {
-    document.querySelector("h1").innerHTML = "It's a tie!"
-} else if (randomNumberOne > randomNumberTwo) {
-    document.querySelector("h1").innerHTML = "Player One Wins!"
-} else {
-    document.querySelector("h1").innerHTML = "Player Two Wins!"
-}
+
+document.querySelector(".btn").addEventListener("click", function (){
+    console.log("I was clicked")
+    let randomNumberOne = Math.floor(Math.random() * 6) + 1
+    let randomNumberTwo = Math.floor(Math.random() * 6) + 1
+    rollMe(randomNumberOne, ".img1");
+    rollMe(randomNumberTwo, ".img2");
+
+    if (randomNumberOne === randomNumberTwo) {
+        document.querySelector("h1").innerHTML = "It's a tie!"
+    } else if (randomNumberOne > randomNumberTwo) {
+        document.querySelector("h1").innerHTML = "Player One Wins!"
+    } else {
+        document.querySelector("h1").innerHTML = "Player Two Wins!"
+    }
+})
